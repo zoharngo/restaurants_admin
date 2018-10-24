3dfa3e4a61c84479c7513225cdc9598d2b57c861
zoharngo <zoharngo@gmail.com>
2018-10-24 11:36:09 +0300

    add entrypoiny script

---
+++ scripts/test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Activate virtual environment
+. /appenv/bin/activate
+
+# Download requirements to build cache
+pip download -d /build -r requirements_test.txt --no-input 
+
+# Install application test requirements
+pip install --no-index -f /build -r requirements_test.txt
+
+# Wait until DB service is ready
+exec $@
+
+
+
